bfb2ce80fd43516e66c15aa7f281125b0c35b31a
cheshire <fantasyofwhiteness@gmail.com>
2021-01-16 12:53:32 +0100

    added data migrations

---
+++ Data/DataMigrations/20210116115213_InitialMigration.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CarRentalApp.Data.DataMigrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Car",
+                columns: table => new
+                {
+                    VIN = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    ModelId = table.Column<int>(nullable: false),
+                    MileageKm = table.Column<int>(nullable: false),
+                    ProductionDate = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Car", x => x.VIN);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CarModel",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Manufacturer = table.Column<string>(nullable: false),
+                    Model = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CarModel", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Rental",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    CarID = table.Column<int>(nullable: false),
+                    UserID = table.Column<int>(nullable: false),
+                    Price = table.Column<int>(nullable: false),
+                    StartDate = table.Column<DateTime>(nullable: false),
+                    EndDate = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Rental", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Car");
+
+            migrationBuilder.DropTable(
+                name: "CarModel");
+
+            migrationBuilder.DropTable(
+                name: "Rental");
+        }
+    }
+}

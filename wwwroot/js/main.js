9d49c3323d2812b3d8f4508407244958969e512f
Kubokoo <Kubokoo@users.noreply.github.com>
2021-01-17 11:23:08 +0100

    Razor Pages sort

--- wwwroot/js/main.js
+++ wwwroot/js/main.js
@@ -1,4 +1,24 @@
-﻿function sortTable(n) {
+﻿function filter() {
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("dynamicTable");
+    tr = table.getElementsByTagName("tr");
+
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[0];
+        if (td) {
+            txtValue = td.textContent || td.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+        }
+    }
+}
+
+function sortTable(n) {
     var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
     table = document.getElementById("dynamicTable");
     switching = true;
